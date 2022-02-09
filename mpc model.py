3681e1d53fc48237a8a59714b30ea6af7acc3a15
Mohab Ali <mohabsir00@gmail.com>
2022-02-09 18:25:46 +0200

    finished simulation loop

--- mpc model.py
+++ mpc model.py
@@ -1,24 +1,29 @@
+from math import exp
 from msilib.schema import Upgrade
+from sqlite3 import Time
 from time import time
 import casadi as ca
 import numpy as np
 from casadi import sin, cos, pi
+from simulation_code import simulate
 
-# weights for control and state variables
-Q_x = 9
-Q_y = 7
-Q_theta = 1
-R1 = 0.05
-R2 = 0.05
-R3 = 0.05
-R4 = 0.05
+# q and r weights ffor state and controls
 
-step_horizon = 0.2  # time between steps in seconds
-N = 20              # number of look ahead steps
+Q_X = 100
+Q_Y = 100
+Q_theta = 2000
+
+R_omega1 = 1
+R_omega2 = 1
+R_omega3 = 1
+R_omega4 = 1
+
+step_horizon = 0.1  # time between steps in seconds
+N = 10              # number of look ahead steps
 rob_diam = 0.3      # diameter of the robot
 wheel_radius = 0.1    # wheel radius
-Lx = 0.2            # L in J Matrix (half robot x-axis length)
-Ly = 0.1            # l in J Matrix (half robot y-axis length)
+Lx = 0.3              # L in J Matrix (half robot x-axis length)
+Ly = 0.3            # l in J Matrix (half robot y-axis length)
 sim_time = 200      # simulation time (will be removed in practical application)
 
 # initial and target position
@@ -31,7 +36,7 @@ theta_target = pi/4
 
 # omega constraints
 omega_max = pi
-omega_min = -pi
+omega_min = -omega_max
 
 # state symbolic variables
 x = ca.SX.sym('x')
@@ -87,10 +92,10 @@ U = ca.SX.sym('U', n_controls, N)
 P = ca.SX.sym('P', n_states + n_states)
 
 # state weights matrix (Q_X, Q_Y, Q_THETA)
-Q = ca.diagcat(Q_x, Q_y, Q_theta)
+Q = ca.diagcat(Q_X, Q_Y, Q_theta)
 
 # controls weights matrix
-R = ca.diagcat(R1, R2, R3, R4)
+R = ca.diagcat(R_omega1, R_omega2, R_omega3, R_omega4)
 
 cost_fn = 0  # cost function
 g = X[:, 0] - P[:n_states]  # constraints in the equation
@@ -138,9 +143,14 @@ solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
 lbx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
 ubx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
 
-# lower and upper bound for constraint
+# lower and upper bound for constraint; they are zero because we have an equality constraint of x(k+1) - x(K+1)rk = 0
 lbg = ca.DM.zeros((n_states*(N+1), 1))
 ubg = ca.DM.zeros((n_states*(N+1), 1))
+
+# INDEXING:
+# we place x, y and theta bounds once every n_states steps so that the matrix has x, y and theta repeated N+1 times (1 set for each time step)
+# e.g. N = 5 
+# [x,y,theta,x,y,theta,x,y,theta,x,y,theta,x,y,theta,x,y,theta]
 
 lbx[0: n_states*(N+1): n_states] = -ca.inf     # X lower bound
 lbx[1: n_states*(N+1): n_states] = -ca.inf     # Y lower bound
@@ -150,13 +160,24 @@ ubx[0: n_states*(N+1): n_states] = ca.inf      # X upper bound
 ubx[1: n_states*(N+1): n_states] = ca.inf      # Y upper bound
 ubx[2: n_states*(N+1): n_states] = ca.inf      # theta upper bound
 
-lbx[n_states*(N+1):] = omega_min
-lbx[n_states*(N+1)+1:] = omega_min
-lbx[n_states*(N+1)+2:] = omega_min
 
-ubx[n_states*(N+1):] = omega_max
-ubx[n_states*(N+1)+1:] = omega_max
-ubx[n_states*(N+1)+2:] = omega_max
+
+# INDEXING:
+# we place omega bounds once every n_controls steps so that the matrix has omega 1-4 repeated N times (1 set for each time step)
+# difference here is that controls start from the end of states; states end at n_states*(N+1), controls start from there and end at n_states*(N+1)+n_controls*N
+# states are N+1 while controls are N because u can get one extra state from controls but not vice versa, so there is always 1 more set of states than controls in multiple shooting
+# e.g. N = 5 
+# [x,y,theta,x,y,theta,x,y,theta,x,y,theta,x,y,theta,x,y,theta,w1,w2,w3,w4,w1,w2,w3,w4,w1,w2,w3,w4,w1,w2,w3,w4,w1,w2,w3,w4]
+
+lbx[n_states*(N+1):n_states*(N+1)+n_controls*N:n_controls] = omega_min    # Omega 1 lower bound
+lbx[n_states*(N+1)+1:n_states*(N+1)+n_controls*N:n_controls] = omega_min  # Omega 2 lower bound 
+lbx[n_states*(N+1)+2:n_states*(N+1)+n_controls*N:n_controls] = omega_min  # Omega 3 lower bound 
+lbx[n_states*(N+1)+3:n_states*(N+1)+n_controls*N:n_controls] = omega_min  # Omega 4 lower bound 
+
+ubx[n_states*(N+1):n_states*(N+1)+n_controls*N:n_controls] = omega_max    # Omega 1 upper bound
+ubx[n_states*(N+1)+1:n_states*(N+1)+n_controls*N:n_controls] = omega_max  # Omega 2 upper bound 
+ubx[n_states*(N+1)+2:n_states*(N+1)+n_controls*N:n_controls] = omega_max  # Omega 3 upper bound 
+ubx[n_states*(N+1)+3:n_states*(N+1)+n_controls*N:n_controls] = omega_max  # Omega 4 upper bound 
 
 args = {
     'lbg': lbg,
@@ -171,7 +192,87 @@ state_init = ca.DM([x_init, y_init, theta_init])        # initial state
 state_target = ca.DM([x_target, y_target, theta_target])  # target state
 
 
-t = ca.DM(t0) # keeps track of time step horizons
+t = ca.DM(t0) # keeps track of time for each iteration
 
-u0 = ca.DM.zeros((n_controls, N))  # initial control
-X0 = ca.repmat(state_init, 1, N+1)         # initial state full
+current_control = ca.DM.zeros((n_controls, N))  # initial control
+current_state = ca.repmat(state_init, 1, N+1)         # initial state full
+target_state = ca.repmat(state_target, 1, N+1)         # target state full
+
+mpc_iteration = 0 # current iteration of system
+
+predicted_states = np.array(current_state.full()) # storing all predicted states in a 3d matrix
+computed_control = np.array(current_control[:,0].full()) # storing all control signals in 2d matrix
+
+times = np.array([[0]]) # keeping track of time
+
+
+# shift function used to set nxt state to be current state and to apply control action
+def shift(t0, state, control, f):
+    f_val = f(state, control[:,0])
+    next_state = ca.DM(state + step_horizon*f_val)
+
+    t0 += step_horizon
+    u0 = ca.horzcat( # removing first control sequence as it is already done and duplicating final control sequence as a prediction
+        control[:,1:],
+        control[:,-1]
+    )
+    return t0, next_state, u0
+
+
+# simulation loop is running while accepted error has not been met and time has not reached max simulation time
+# if either condition has been met the loop terminates 
+while ca.norm_fro(state_target-state_init) > 10**-2 and mpc_iteration < sim_time / step_horizon :
+    t1 = time() # getting time at teh start of iteration
+
+    args['p'] = ca.vertcat(         # passing in the current state and the reference, target, state to the solver
+        state_init,
+        state_target
+    )
+
+    args['x0'] = ca.vertcat(        # passing in the current optimization variables, state and control, to the solver
+        ca.reshape(current_state, n_states*(N+1), 1),
+        ca.reshape(current_control, n_controls*N, 1)
+    )
+
+    # running the solver to get optimization variables, state and control
+    sol = solver(x0=args['x0'], lbx=args['lbx'], ubx=args['ubx'], lbg=args['lbg'], ubg=args['ubg'], p=args['p'])
+
+    control = ca.reshape(sol['x'][n_states*(N+1):], n_controls, N) # extracting control actions from solution and shaping it as a matrix instead of a vector
+    current_state = ca.reshape(sol['x'][:n_states*(N+1)], n_states, N+1) # extracting predicted states from solution and shaping it as a matrix instead of a vector
+
+    computed_control = np.vstack(( # adding first calculated control sequence (storing applied control sequence)
+        computed_control,
+        np.array(control[:,0].full())
+    ))
+
+    predicted_states = np.dstack(( # adding calculated state sequence, using dstack to store it depth wise (3d)
+        predicted_states,
+        np.array(current_state.full())
+    ))
+
+    t0, state_init, current_control = shift(t0, state_init, control, f)  # shifting to get next state and apply control action
+
+    t = ca.vertcat( # adding timestep to total
+        t,
+        t0
+    )
+
+    current_state = ca.horzcat( # shifting current state by one and again duplicating last state
+        current_state[:, 1:],
+        current_state[:,-1]
+    )
+
+    t2 = time() # geting time after iteration
+
+    times = np.vstack(( # storing time taken for current iteration
+        times,
+        t2-t1
+    ))
+
+    mpc_iteration += 1 # incrementeing iteration count
+
+
+# visualizing system
+simulate(predicted_states, computed_control, times, step_horizon, N, 
+            np.array([x_init, y_init, theta_init, x_target, y_target, theta_target]))
+    
